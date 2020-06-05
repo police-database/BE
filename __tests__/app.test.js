dffdc81415b181db27479d61bfe01f01bcd91973
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-06-05 11:14:45 -0700

    initial commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,23 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('back-end routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+});
